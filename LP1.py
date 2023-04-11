ae6e177374f573ea46e6786ef8486acea252f6ab
Flavio Munck <flavioives23@gmail.com>
2023-04-11 10:02:00 -0300

    Create LP1.py

---
+++ LP1.py
@@ -0,0 +1,32 @@
+# Calculos Geométricos
+#1. Defina uma função que calcule a  área de um retângulo dados seus dois lados. Teste pelo menos para os
+# seguintes pares de entrada:
+• 5 e 7; resposta esperada  ́e 35
+• 15 e 2; resposta esperada  ́e 30
+• 500 e 700; resposta esperada  ́e 350000
+#
+def geocalc(a,b):
+  return a * b
+
+geocalc(5,7)
+geocalc(15,2)
+geocalc(500,700)
+
+# 2. Defina uma função que calcule a área da superfície de um cubo que tem c por aresta.'''
+def areacubo(c):
+  return c ** 2
+
+areacubo()
+
+
+
+
+
+# 4. 
+def parescalc(a,b):
+  return (a + b) / 2
+parescalc(-5,7)
+parescalc(2, -2)
+parescalc(5, 5)
+parescalc(3, 4)
+parescalc(3.0, 4.0)
