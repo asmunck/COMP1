c98160d57876beee8fdbc804b372688081988a56
Flavio Munck <flavioives23@gmail.com>
2023-04-25 09:29:39 -0300

    Update aula_2504.py

--- aula_2504.py
+++ aula_2504.py
@@ -1,3 +1,4 @@
+# 1.
 def habilitacao(idade, pratica, teorica):
   if idade < 18:
     print("Inapto por idade.")
@@ -9,3 +10,10 @@ def habilitacao(idade, pratica, teorica):
     print("Aprovado.")
 
 habilitacao(18, True, False)
+
+# 2.
+def trianguloisosceles(lado1, lado2, lado3):
+  if lado1 == lado2 and lado3 != lado1:
+    print("Triângulo isosceles")
+  else:
+    print("Triângulo não isosceles.")
